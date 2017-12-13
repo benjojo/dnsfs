9b804063c03e372ff5101bcd51f058b32cdeef79
Ben Cartwright-Cox <Ben@Benjojo.co.uk>
2017-12-13 23:55:07 +0000

    not really sure if I am honest

--- dnsfs/dns.go
+++ dnsfs/dns.go
@@ -2,8 +2,85 @@ package main
 
 import (
 	"net"
+
+	"strings"
+
+	"log"
+
+	"github.com/miekg/dns"
 )
 
 func startDNSListener(socket net.PacketConn) {
+	go DNSLoop(socket)
+}
 
+type storageRequest struct {
+	storageNotifications chan bool
+	content              string
+	replications         int
+}
+
+var uploadPendingMap map[string]storageRequest
+
+func DNSLoop(socket net.PacketConn) {
+	for {
+		dnsin := make([]byte, 1500)
+		inbytes, inaddr, err := socket.ReadFrom(dnsin)
+
+		inmsg := &dns.Msg{}
+
+		if unpackErr := inmsg.Unpack(dnsin[inbytes:]); unpackErr != nil {
+			log.Printf("Unable to unpack DNS request %s", err.Error())
+			continue
+		}
+
+		if len(inmsg.Question) != 1 {
+
+			log.Printf("More than one quesion in query (%d), droppin %+v", len(inmsg.Question), inmsg)
+			continue
+		}
+
+		if !strings.Contains(inmsg.Question[0].Name, *dnsbase) {
+			log.Printf("question is not for us '%s' vs expected '%s'", inmsg.Question[0].Name, *dnsbase)
+
+			continue
+		}
+
+		outmsg := &dns.Msg{}
+
+		ostring := make([]string, 1)
+		ostring[0] = "aaaaaaaaaaaaaaaaaaaaa"
+
+		inmsg.SetReply(outmsg)
+		outmsg.Answer = make([]dns.RR, 1)
+		outmsg.Answer[0] = &dns.TXT{
+			Hdr: dns.RR_Header{
+				Name:   inmsg.Question[0].Name,
+				Rrtype: dns.TypeTXT,
+				Class:  dns.ClassINET,
+				Ttl:    2147483646},
+			Txt: ostring,
+		}
+		outputb, err := outmsg.Pack()
+
+		if err != nil {
+			log.Printf("unable to pack response to thing")
+			continue
+		}
+
+		socket.WriteTo(outputb, inaddr)
+	}
+}
+
+func verifyNSsetup(name string) bool {
+	s, err := net.LookupTXT("tokentest" + name)
+	if err != nil {
+		return false
+	}
+
+	if len(s) != 1 {
+		return false
+	}
+
+	return true
 }
--- dnsfs/main.go
+++ dnsfs/main.go
@@ -23,39 +23,13 @@ func main() {
 		log.Fatalf("failed to listen on UDP 53 %s", err.Error())
 	}
 
-	if !verifyNSsetup(*dnsbase, *baddr) {
-		log.Fatalf("failed to confirm NS records are setup correctly, exiting")
-	}
-
 	startDNSListener(rxListener)
+
+	// if !verifyNSsetup(*dnsbase) {
+	// 	log.Fatalf("failed to confirm NS records are setup correctly, exiting")
+	// }
 
 	http.HandleFunc("/upload", handleUpload)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatalf("Failed to listen on HTTP: %s", http.ListenAndServe(*baddr+":5050", nil))
-}
-
-func verifyNSsetup(name, ipaddr string) bool {
-	nsr, err := net.LookupNS(name)
-	if err != nil {
-		log.Printf("Unable to check NS setup correctly (1): %s", err.Error())
-		return false
-	}
-
-	if len(nsr) != 1 {
-		log.Printf("NS setup incorrect, I saw %d NS records for %s, I am expecting a single one.", len(nsr), name)
-		return false
-	}
-
-	ar, err := net.LookupIP(nsr[0].Host)
-	if len(ar) != 1 {
-		log.Printf("NS setup incorrect, I saw %d A/AAAA records for %s, I am expecting a single one.", len(ar), nsr[0].Host)
-		return false
-	}
-
-	if ar[0].String() != net.ParseIP(ipaddr).String() {
-		log.Printf("NS setup incorrect, %s is suppose to point to %s, but it points to %s", nsr[0].Host, ipaddr, ar[0].String())
-		return false
-	}
-
-	return true
 }
